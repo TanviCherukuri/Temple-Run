9d8bd1c2e7c334805f48465d4660fdf7ce82fdaa
TanviCherukuri <tanvi.orby@gmail.com>
2021-01-26 22:03:10 -0500

    Change1

--- sketch.js
+++ sketch.js
@@ -3,9 +3,15 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world;
+var engine, world, monkeyAnimation, coinIMG, jungleIMG, caveEntranceIMG, stoneIMG, quicksandIMG;
 
 function preload() {
+    monkeyAnimation = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+    coinIMG = loadImage("Coin.png");
+    jungleIMG = loadImage("jungle.jpg");
+    caveEntranceIMG = loadImage("Cave Entrance.jpg");
+    stoneIMG = loadImage("stone.png");
+    quicksandIMG = loadImage("Quicksand.png");
 }
 
 function setup(){
